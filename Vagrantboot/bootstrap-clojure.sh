e679d180a915781c39606879e1ae7763a5ace253
Howard Abrams <howard.abrams@workday.com>
2015-03-27 13:46:49 -0700

    Add a comment to explain the kludge

--- Vagrantboot/bootstrap-clojure.sh
+++ Vagrantboot/bootstrap-clojure.sh
@@ -19,8 +19,14 @@ then
     ln -s /vagrant/yesql-xp $HOME
 fi
 
+# According to the discussion on this page:
+#   http://stackoverflow.com/questions/28688721/how-to-upgrade-nrepl-version-of-leiningen
+# We can currently resolve the 2.6 warning by updating the user
+# accounts' profiles.clj file, not just the project.clj
+
 if [ ! -e $HOME/.lein/profiles.clj ]
 then
+    mkdir -p $HOME/.lein
     cat > $HOME/.lein/profiles.clj <<EOF
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
